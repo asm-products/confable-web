6821e3158151479cb8230b9d97ddb5408192bd55
Barry Melton <bmelton@lt693.home>
2015-04-20 00:14:13 -0400

    5.

--- models/account.go
+++ models/account.go
@@ -4,130 +4,130 @@ package models
 // "code.google.com/p/go.crypto/bcrypt"
 // "github.com/dgrijalva/jwt-go"
 
-import (
-    "fmt"
-    "math/rand"
-    "os"
-    "regexp"
-    "time"
-)
+// import (
+// "fmt"
+// "math/rand"
+// "os"
+// "regexp"
+// "time"
+// )
 
-var letters = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+// var letters = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
 
-type Account struct {
-    Id                int64     `db:"id" json:"id"`
-    CreatedAt         time.Time `db:"created_at" json:"created"`
-    UpdatedAt         time.Time `db:"updated_at" json:"updated"`
-    Email             string    `db:"email" json:"email"`
-    EncryptedPassword []byte    `db:"encrypted_password" json:"-"`
-    Plan              string    `db:"plan" json:"plan"`
-    Subdomain         string    `db:"subdomain" json:"subdomain"`
-}
+// type Account struct {
+// Id                int64     `db:"id" json:"id"`
+// CreatedAt         time.Time `db:"created_at" json:"created"`
+// UpdatedAt         time.Time `db:"updated_at" json:"updated"`
+// Email             string    `db:"email" json:"email"`
+// EncryptedPassword []byte    `db:"encrypted_password" json:"-"`
+// Plan              string    `db:"plan" json:"plan"`
+// Subdomain         string    `db:"subdomain" json:"subdomain"`
+// }
 
-func FindAccountById(id int64) *Account {
-    accounts, err := Dbm.Select(Account{}, `select * from accounts where id = $1 limit 1`, id)
-    if err != nil {
-        panic(err)
-    }
-    if len(accounts) == 0 {
-        return nil
-    }
-    return accounts[0].(*Account)
-}
+// func FindAccountById(id int64) *Account {
+// accounts, err := Dbm.Select(Account{}, `select * from accounts where id = $1 limit 1`, id)
+// if err != nil {
+// panic(err)
+// }
+// if len(accounts) == 0 {
+// return nil
+// }
+// return accounts[0].(*Account)
+// }
 
-func FindAccountByJwt(jwtString string) *Account {
-    token, err := jwt.Parse(jwtString, func(token *jwt.Token) (interface{}, error) {
-        return []byte(os.Getenv("SECRET")), nil
-    })
-    if err != nil || !token.Valid {
-        panic(err)
-    }
+// func FindAccountByJwt(jwtString string) *Account {
+// token, err := jwt.Parse(jwtString, func(token *jwt.Token) (interface{}, error) {
+// return []byte(os.Getenv("SECRET")), nil
+// })
+// if err != nil || !token.Valid {
+// panic(err)
+// }
 
-    account := FindAccountById(int64(token.Claims["account_id"].(float64)))
-    if err != nil {
-        panic(err)
-    }
+// account := FindAccountById(int64(token.Claims["account_id"].(float64)))
+// if err != nil {
+// panic(err)
+// }
 
-    return account
-}
+// return account
+// }
 
-func FindAccountByEmail(email string) *Account {
-    accounts, err := Dbm.Select(Account{}, `select * from accounts where lower(email) = lower($1) limit 1`, email)
-    if err != nil {
-        panic(err)
-    }
-    if len(accounts) == 0 {
-        return nil
-    }
-    return accounts[0].(*Account)
-}
+// func FindAccountByEmail(email string) *Account {
+// accounts, err := Dbm.Select(Account{}, `select * from accounts where lower(email) = lower($1) limit 1`, email)
+// if err != nil {
+// panic(err)
+// }
+// if len(accounts) == 0 {
+// return nil
+// }
+// return accounts[0].(*Account)
+// }
 
-func FindAccountBySubdomain(subdomain string) *Account {
-    accounts, err := Dbm.Select(Account{}, `select * from accounts where subdomain = $1 limit 1`, subdomain)
-    if err != nil {
-        panic(err)
-    }
-    if len(accounts) == 0 {
-        return nil
-    }
-    return accounts[0].(*Account)
-}
+// func FindAccountBySubdomain(subdomain string) *Account {
+// accounts, err := Dbm.Select(Account{}, `select * from accounts where subdomain = $1 limit 1`, subdomain)
+// if err != nil {
+// panic(err)
+// }
+// if len(accounts) == 0 {
+// return nil
+// }
+// return accounts[0].(*Account)
+// }
 
-func (a *Account) String() string {
-    return fmt.Sprintf("Account(%a)", a.Email)
-}
+// func (a *Account) String() string {
+// return fmt.Sprintf("Account(%a)", a.Email)
+// }
 
-func (a *Account) GenEncryptedPassword(password string) error {
-    var err error
-    a.EncryptedPassword, err = bcrypt.GenerateFromPassword(
-        []byte(password), bcrypt.DefaultCost)
-    return err
-}
+// func (a *Account) GenEncryptedPassword(password string) error {
+// var err error
+// a.EncryptedPassword, err = bcrypt.GenerateFromPassword(
+// []byte(password), bcrypt.DefaultCost)
+// return err
+// }
 
-func (a *Account) GenJwt() (string, error) {
-    token := jwt.New(jwt.GetSigningMethod("HS256"))
-    token.Claims["account_id"] = a.Id
-    token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
-    return token.SignedString([]byte(os.Getenv("SECRET")))
-}
+// func (a *Account) GenJwt() (string, error) {
+// token := jwt.New(jwt.GetSigningMethod("HS256"))
+// token.Claims["account_id"] = a.Id
+// token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+// return token.SignedString([]byte(os.Getenv("SECRET")))
+// }
 
-func (a *Account) GenSubdomain() {
-    if a.Subdomain != "" {
-        return
-    }
+// func (a *Account) GenSubdomain() {
+// if a.Subdomain != "" {
+// return
+// }
 
-    randomLetters := make([]rune, 12)
-    for i := range randomLetters {
-        randomLetters[i] = letters[rand.Intn(len(letters))]
-    }
+// randomLetters := make([]rune, 12)
+// for i := range randomLetters {
+// randomLetters[i] = letters[rand.Intn(len(letters))]
+// }
 
-    a.Subdomain = string(randomLetters)
-}
+// a.Subdomain = string(randomLetters)
+// }
 
-func (a *Account) FiresizeUrl() string {
-    return fmt.Sprintf("https://%s.firesize.com", a.Subdomain)
-}
+// func (a *Account) FiresizeUrl() string {
+// return fmt.Sprintf("https://%s.firesize.com", a.Subdomain)
+// }
 
-func (a *Account) Serialize() map[string]interface{} {
-    // now := time.Now()
-    // month := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
+// func (a *Account) Serialize() map[string]interface{} {
+// // now := time.Now()
+// month := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
 
-    // requests, err := Dbm.Select(ImageRequest{}, `SELECT * FROM image_requests WHERE account_id = $1 AND DATE_TRUNC('month', created_at) = $2 ORDER BY image_requests.created_at DESC`, a.Id, month)
-    // if err != nil {
-        // panic(err)
-    // }
+// requests, err := Dbm.Select(ImageRequest{}, `SELECT * FROM image_requests WHERE account_id = $1 AND DATE_TRUNC('month', created_at) = $2 ORDER BY image_requests.created_at DESC`, a.Id, month)
+// if err != nil {
+// panic(err)
+// }
 
-    return map[string]interface{}{
-        "subdomain":              a.Subdomain,
-        "plan":                   a.Plan,
-        "plan_limit":             100,
-        // "request_count":          len(requests),
-        // "requests":               requests,
-        "only_allow_whitelisted": true,
-    }
-}
+// return map[string]interface{}{
+// "subdomain":              a.Subdomain,
+// "plan":                   a.Plan,
+// "plan_limit":             100,
+// "request_count":          len(requests),
+// "requests":               requests,
+// "only_allow_whitelisted": true,
+// }
+// }
 
-var emailRegex = regexp.MustCompile("^.+@.+$")
+// var emailRegex = regexp.MustCompile("^.+@.+$")
 
 // func (a *Account) Validate(v *revel.Validation) {
 //  v.Check(a.Email,
--- models/db.go
+++ models/db.go
@@ -10,28 +10,28 @@ import (
     "os"
 )
 
-var Dbm *gorp.DbMap
+// var Dbm *gorp.DbMap
 
 // Init creates a connection to postgres and returns a gorp
-func InitDb(url string) {
-    db, err := sql.Open("postgres", url)
-    if err != nil {
-        panic(err)
-    }
+// func InitDb(url string) {
+// db, err := sql.Open("postgres", url)
+// if err != nil {
+// panic(err)
+// }
 
-    Dbm = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
-    Dbm.AddTableWithName(Account{}, "accounts").SetKeys(true, "Id")
-    Dbm.TraceOn("[gorp]", log.New(os.Stdout, "sql:", log.Lmicroseconds))
-}
+// Dbm = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+// Dbm.AddTableWithName(Account{}, "accounts").SetKeys(true, "Id")
+// Dbm.TraceOn("[gorp]", log.New(os.Stdout, "sql:", log.Lmicroseconds))
+// }
 
-func Insert(m interface{}) error {
-    return Dbm.Insert(m)
-}
+// func Insert(m interface{}) error {
+// return Dbm.Insert(m)
+// }
 
-func Update(m interface{}) (count int64, err error) {
-    return Dbm.Update(m)
-}
+// func Update(m interface{}) (count int64, err error) {
+// return Dbm.Update(m)
+// }
 
-func Delete(m interface{}) (count int64, err error) {
-    return Dbm.Delete(m)
-}
+// func Delete(m interface{}) (count int64, err error) {
+// return Dbm.Delete(m)
+// }
